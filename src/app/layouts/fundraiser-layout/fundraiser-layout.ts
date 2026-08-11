40f68b4d7ebd238e0c74e6ab92f54aad7b744c3d
pavala <pavala@miraclesoft.com>
2026-08-11 17:48:31 +0530

    Add rich global CSS styling system and component designs

--- src/app/layouts/fundraiser-layout/fundraiser-layout.ts
+++ src/app/layouts/fundraiser-layout/fundraiser-layout.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-fundraiser-layout',
@@ -7,4 +8,11 @@ import { RouterLink } from "@angular/router";
   templateUrl: './fundraiser-layout.html',
   styleUrl: './fundraiser-layout.css',
 })
-export class FundraiserLayout { }
+export class FundraiserLayout {
+  constructor(private as: Auth, private router: Router) { }
+
+  logout() {
+    this.as.logout();
+    this.router.navigate(['/login']);
+  }
+}
